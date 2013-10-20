201cbb5dc8d2e9639dc7fa66b979e1adc6e927c3
Rich Heimann <heimann.richard@gmail.com>
2013-10-19 21:23:23 -0400

    sentiment.R
    sentiment.R

---
+++ sentiment.R
@@ -0,0 +1,24 @@
+score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
+{
+	require(plyr)
+	require(stringr)
+
+	scores = laply(sentences, function(sentence, pos.words, neg.words) {
+    
+		word.list = str_split(sentence, '\\s+')
+		words = unlist(word.list)
+    
+		pos.matches = match(words, pos.words)
+		neg.matches = match(words, neg.words)
+	
+		pos.matches = !is.na(pos.matches)
+		neg.matches = !is.na(neg.matches)
+
+		score = sum(pos.matches) - sum(neg.matches)
+
+		return(score)
+	}, pos.words, neg.words, .progress=.progress )
+
+	scores.df = data.frame(score=scores, text=sentences)
+	return(scores.df)
+}
